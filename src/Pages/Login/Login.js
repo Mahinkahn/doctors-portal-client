d9a7bbfb07c4278f4267a5bd273cb58e360366ea
Mahinkahn <mahinkahn37@gmail.com>
2022-12-22 20:46:53 +0600

    spne

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const { register, handleSubmit } = useForm()
+    const { register, formState: { errors }, handleSubmit } = useForm()
 
     const handleLogin = data => {
         console.log(data);
@@ -18,16 +18,19 @@ const Login = () => {
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" {...register("email")} className="input input-bordered w-full max-w-xs" />
+                        <input type="email" {...register("email", { required: "Email Addrss is required" })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
+                        <input type="password" {...register("password", { required: "password is required", minLength: { value: 6, message: 'Password must be 6 characters or longer' } })}
+                            className="input input-bordered w-full max-w-xs" />
                         <label className="label">
                             <span className="label-text">Forget Password</span>
                         </label>
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
                     </div>
                     <input className='btn btn-accent w-full' value="Login" type="submit" />
                 </form>
